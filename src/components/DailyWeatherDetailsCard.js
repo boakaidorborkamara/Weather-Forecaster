32180b7aa98f5e5be25d81029bbb027492024aa3
boakaidkamara <boakaidkamara@gmail.com>
2023-09-28 00:06:35 +0000

    display five days forecast details to the DOM

--- src/components/DailyWeatherDetailsCard.js
+++ src/components/DailyWeatherDetailsCard.js
@@ -2,19 +2,16 @@ import React from "react";
 import cloud from "../assests/cloud.png";
 
 function DailyWeatherDetailsCard({ day_details }) {
-  console.log("day details", day_details);
-  // console.log(day.datetime, day.icon, day.tempmin, day.tempmax);
-
   return (
     <div className="weather-details-card py-4 m-2">
       <div className="">
-        <p>Friday</p>
-        <img src={cloud} className="card-image" />
+        <p>{day_details.datetime}</p>
+        <img src={day_details.icon} className="card-image" />
       </div>
       <div className="d-flex text-center align-item-center justify-content-center mt-5">
-        <p>31°C</p>
+        <p>{day_details.tempmin}°C</p>
         <span className="mx-3"></span>
-        <p>78°C</p>
+        <p>{day_details.tempmax}°C</p>
       </div>
     </div>
   );
