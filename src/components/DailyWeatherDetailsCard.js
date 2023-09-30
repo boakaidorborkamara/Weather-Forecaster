87a35ce85b3c1e1a8768436f7ce6ed6873e3ef85
boakaidkamara <boakaidkamara@gmail.com>
2023-09-30 23:59:03 +0000

    Fix bug when rendering weather icon dynamicly

--- src/components/DailyWeatherDetailsCard.js
+++ src/components/DailyWeatherDetailsCard.js
@@ -1,12 +1,18 @@
 import React from "react";
 import cloud from "../assests/cloud.png";
+import rain from "../assests/rain.png";
 
 function DailyWeatherDetailsCard({ day_details }) {
+  // function setIcon(image_name){
+  //   import image_name from "../assests/rain.png";
+
+  // }
+
   return (
     <div className="weather-details-card py-4 m-2">
       <div className="">
         <p>{day_details.datetime}</p>
-        <img src={`../assets/rain.png`} className="card-image" />
+        <img src={require("../assests/rain.png")} className="card-image" />
       </div>
       <div className="d-flex text-center align-item-center justify-content-center mt-5">
         <p>{day_details.tempmin}°C</p>
